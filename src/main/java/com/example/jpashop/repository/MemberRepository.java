4d60215f7a5ffd22533a7cafa7db9d2ff980c3e1
wisekim <lwisekiml@naver.com>
2024-02-28 22:06:07 +0900

    회원 서비스 개발
    일관성을 위해 MemberRepository도 수정

--- src/main/java/com/example/jpashop/repository/MemberRepository.java
+++ src/main/java/com/example/jpashop/repository/MemberRepository.java
@@ -2,16 +2,16 @@ package com.example.jpashop.repository;
 
 import com.example.jpashop.domain.Member;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
---
+++ src/main/java/com/example/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package com.example.jpashop.service;
+
+import com.example.jpashop.domain.Member;
+import com.example.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 모든 데이터 변경이나 어떤 로직들은 가급적이면 트랜잭션 안에서 실행되어야 한다. readOnly를 사용하면 읽는 것을 수행할 때 최적화됨
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional // readOnly가 아니다.
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
