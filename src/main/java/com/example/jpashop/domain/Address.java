474e4cc9784f6aaa915962439588ff29b8ec26b7
wisekim <lwisekiml@naver.com>
2024-02-28 16:36:32 +0900

    엔티티 클래스 개발2
    category_item 관련 테이블 설정
    Address는 생성할 때만 값을 세팅할 수 있도록 수정

--- src/main/java/com/example/jpashop/domain/Address.java
+++ src/main/java/com/example/jpashop/domain/Address.java
@@ -3,6 +3,11 @@ package com.example.jpashop.domain;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 
+/*
+JPA 스펙상 엔티티나 임베디드 타입( @Embeddable )은 자바 기본 생성자(default constructor)를 public 또는 protected 로 설정해야 한다.
+public 으로 두는 것 보다는 protected 로 설정하는 것이 그나마 더 안전하다.
+JPA가 이런 제약을 두는 이유는 JPA 구현 라이브러리가 객체를 생성할 때 리플랙션 같은 기술을 사용할 수 있도록 지원해야 하기 때문이다.
+ */
 @Embeddable // jpa 내장 타입
 @Getter
 public class Address {
@@ -10,4 +15,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() { // reflection, proxy 같은 기술을 써야 될때가 많다. 기본 생성자가 없으면 못하기에 필요하다.
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/com/example/jpashop/domain/item/Item.java
+++ src/main/java/com/example/jpashop/domain/item/Item.java
@@ -1,8 +1,12 @@
 package com.example.jpashop.domain.item;
 
+import com.example.jpashop.domain.Category;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 싱글 테이블 전략
@@ -16,4 +20,7 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
