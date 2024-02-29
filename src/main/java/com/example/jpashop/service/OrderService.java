dcc69164ef4968ef6cd0030d2260ce3ba7084e95
wisekim <lwisekiml@naver.com>
2024-02-29 10:05:56 +0900

    주문 서비스 개발 - 주문
    - Order만 Delivery와 OrderItem을 사용해서 cascade를 사용했다. 다른데서도 사용한다면 cacade 사용은 고민해봐야 한다.

---
+++ src/main/java/com/example/jpashop/service/OrderService.java
@@ -0,0 +1,59 @@
+package com.example.jpashop.service;
+
+import com.example.jpashop.domain.Delivery;
+import com.example.jpashop.domain.Member;
+import com.example.jpashop.domain.Order;
+import com.example.jpashop.domain.OrderItem;
+import com.example.jpashop.domain.item.Item;
+import com.example.jpashop.repository.ItemRepository;
+import com.example.jpashop.repository.MemberRepository;
+import com.example.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송 정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order); // Order에 보면 Delivery와 OrderItem에 cascade 적용되어 있어 따로 save 하지 않아도 된다.
+
+        return order.getId();
+    }
+
+    /**
+     * 주문 조회
+     */
+
+    /**
+     * 주문 검색 조회
+     */
+
+}
