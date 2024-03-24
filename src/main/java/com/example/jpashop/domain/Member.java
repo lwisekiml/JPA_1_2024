d48dfc8a963459f4afa05405a39aa3d141ca6e55
wisekim <lwisekiml@naver.com>
2024-03-24 21:29:18 +0900

    회원 조회 API : v1
    - 회원 조회인데 order가 나와서 @JsonIgnore를 사용해서 안나오게 수정했지만 회원 조회와 관련된 api가 한두가지가 아닐 것이다. 굉장히 많은 클라이언트들이 다양한 api 스타일을 요구할 때 문제가 된다.

--- src/main/java/com/example/jpashop/domain/Member.java
+++ src/main/java/com/example/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package com.example.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
@@ -23,6 +24,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member") // Order에 있는 member에 매핑 되어진 진다. 그래서 여기값이 바뀌어도 외래키값이 변경되지 않는다.
     private List<Order> orders = new ArrayList<>();
 }
