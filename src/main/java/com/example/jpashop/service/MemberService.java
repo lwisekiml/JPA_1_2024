2a05f8436e525b7cc0979e00f5a0f1d184cd77a8
wisekim <lwisekiml@naver.com>
2024-04-05 22:00:28 +0900

    스프링 데이터 JPA 소개

--- src/main/java/com/example/jpashop/service/MemberService.java
+++ src/main/java/com/example/jpashop/service/MemberService.java
@@ -40,12 +40,12 @@ public class MemberService {
     }
 
     public Member findOne(Long memberId) {
-        return memberRepository.findOne(memberId);
+        return memberRepository.findById(memberId).get();
     }
 
     @Transactional
     public void update(Long id, String name) { // member를 넘겨도 되지만 컨맨드와 쿼리를 분리하기 위해 void로 함
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
     }
 }
